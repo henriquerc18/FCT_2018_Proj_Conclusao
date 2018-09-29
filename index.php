97e23aa7ce2203938a88cdce019aae3d578fc598
HenriqueRosaCarvalho <henriquerc2015@gmail.com>
2018-09-29 00:37:12 -0300

    29/09.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Escola Técnica Santo Inácio -->
+<!-- Feira de Ciências e Tecnologia/Projeto de Conclusão de Curso -->
+<!-- Sociedade Espírita Francisco de Assis (SEFA) -->
+<!-- Nome: Henrique Rosa Carvalho e Gabriel Suterio Pereira da Silva -->
+<!-- Data: 03/08/2018 -->
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8" keywords="html5" authors="Henrique Rosa Carvalho, Gabriel Suterio Pereira da Silva" />
+		<link rel="stylesheet" href="validar.php" "captcha.php">
+	</head>
+	<body>
+		
+		<img src="captcha.php?largura=150&altura=50&tamanho_fonte=20&quantidade_letras=5">
+		
+		<form action="validar.php" name="form" method="post">
+		   <input type="text" name="palavra"  />
+		   <input type="submit" value="Validar Captcha" />
+		</form>
+		
+	</body>
+</html>
