9713b822779db3bd6b201629f0b7f558e5264a70
HenriqueRosaCarvalho <henriquerc2015@gmail.com>
2018-11-05 17:44:56 -0200

    05/11.

---
+++ cadastrar.php
@@ -0,0 +1,27 @@
+<!-- Nome: Henrique Rosa Carvalho -->
+<!-- Data: 11/10/2018 -->
+
+
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> Cadastre-se </title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<div> Cadastro de usuário </div>
+			<form action="controle.php" method="post">
+				<label> Login </label><input type="text" name="login" value=""/><br>
+				<label> Senha </label><input type="password" name="senha" value=""/><br>
+				<label> Tipo de usuário </label>
+				<select name="tipo_usuario">
+					<option value=""> Selecione </option>
+					<option value="1"> Usuário Comum </option>
+					<option value="2"> Administrador </option>
+				</select><br>
+				<input type="submit" name="cadastrar" value="cadastrar"/>
+				<a href="cadastrar.php"> Sem cadastro? </a>
+			</form>
+		</body>
+	</html>
