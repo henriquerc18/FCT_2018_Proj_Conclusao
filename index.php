ebdfe9d7e6241916f42e822d14df39082e9ff8e2
Henrique Rosa Carvalho <henriquerc2015@gmail.com>
2018-11-05 22:24:40 -0200

    05/11 22:24 Add Arquivos PHP/MySQL/Banco.

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-<!-- Escola Técnica Santo Inácio -->
-<!-- Feira de Ciências e Tecnologia/Projeto de Conclusão de Curso -->
-<!-- Sociedade Espírita Francisco de Assis (SEFA) -->
-<!-- Nome: Henrique Rosa Carvalho e Gabriel Suterio Pereira da Silva -->
-<!-- Data: 03/08/2018 -->
+<!-- Nome: Henrique Rosa Carvalho -->
+<!-- Data: 11/10/2018 -->
 
-<!DOCTYPE HTML>
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8" keywords="html5" authors="Henrique Rosa Carvalho, Gabriel Suterio Pereira da Silva" />
-	</head>
-	<body>
-		
-		<img src="captcha.php?l=150&a=50&tf=20&ql=5">
-		
-		<form action="validar.php" name="form" method="post">
-		   <input type="text" name="palavra"  />
-		   <input type="submit" value="Validar Captcha" />
-		</form>
-		
-	</body>
-</html>
+
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> Teste de validação DEVMEDIA </title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<div> Login </div>
+			<form action="controle.php" method="post">
+				<input type="text" name="login" value=""/>
+				<input type="password" name="senha" value=""/>
+				<input type="submit" name="enviar" value="Login"/>
+				<a href="cadastrar.php"> Sem cadastro? </a>
+			</form>
+		</body>
+	</html>
